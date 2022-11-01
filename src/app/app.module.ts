6841acce715e11fb6d2856b0c31ced18ce76c61a
camilo <camilo22serna22@gmail.com>
2022-11-01 07:54:20 -0500

    se crearon los primeros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeMainComponent } from './components/home-main/home-main.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+
+
+const appRoutes: Routes = [
+  {path:'', component:HomeMainComponent},
+  {path:'header', component:HeaderComponent },
+  {path:'signin', component: SignInComponent },
+  {path:'signup', component:SignUpComponent },
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeMainComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
